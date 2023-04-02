0c52c70e787d52daec4a31ef6dd744f2bca26419
choikyunho <rbsgh777@naver.com>
2023-04-02 22:24:35 +0900

    components

--- src/App.jsx
+++ src/App.jsx
@@ -1,111 +1,22 @@
-import { useState } from "react";
+import Header from "./components/Header"
+import Portfolio from "./components/Portfolio"
+import Introduce from "./components/Introduce"
+import ContactMe from "./components/ContactMe"
+import footer from "./components/footer"
 
-const App = () => {
-  const [num1, setNum1] = useState();
-  const [num2, setNum2] = useState();
-  const [result, setResult] = useState(0);
-
-  const onChangeNum1 = (e) => {
-    setNum1(e.target.value);
-  };
-  const onChangeNum2 = (e) => {
-    setNum2(e.target.value);
-  };
-  const onClickAdd = () => {
-    if (num1 === "" || num2 === "") {
-      alert("값을 입력해주세요.");
-      return;
-    }
-    if (isNaN(num1) || isNaN(num2)) {
-      alert("숫자를 입력해주세요.");
-      return;
-    }
-
-    setResult(parseInt(num1) + parseInt(num2));
-  };
-  const onClickSubtract = () => {
-    if (num1 === "" || num2 === "") {
-      alert("값을 입력해주세요.");
-      return;
-    }
-    if (isNaN(num1) || isNaN(num2)) {
-      alert("숫자를 입력해주세요.");
-      return;
-    }
-
-    setResult(parseInt(num1) - parseInt(num2));
-  };
-  const onClickMultiply = () => {
-    if (num1 === "" || num2 === "") {
-      alert("값을 입력해주세요.");
-      return;
-    }
-    if (isNaN(num1) || isNaN(num2)) {
-      alert("숫자를 입력해주세요.");
-      return;
-    }
-
-    setResult(parseInt(num1) * parseInt(num2));
-  };
-  const onClickDivide = () => {
-    if (num1 === "" || num2 === "") {
-      alert("값을 입력해주세요.");
-      return;
-    }
-    if (isNaN(num1) || isNaN(num2)) {
-      alert("숫자를 입력해주세요.");
-      return;
-    }
-
-    setResult(parseInt(parseInt(num1) / parseInt(num2)));
-  };
-
+function App() {
   return (
-    <div className="bg-red-100 min-h-screen flex flex-col justify-center items-center">
-      <h1 className="text-2xl font-bold mb-4">Awesome한 계산기(로 바뀔예정)</h1>
-      <div className="text-3xl font-black mb-4">{result}</div>
-      <div className="mb-4">
-        <input
-          className="border-2 focus:outline-none focus:border-purple-400 rounded-lg px-4 py-2 text-xl"
-          type="text"
-          value={num1}
-          onChange={onChangeNum1}
-        />
-        <input
-          className="border-2 focus:outline-none focus:border-purple-400 rounded-lg px-4 py-2 text-xl ml-4"
-          type="text"
-          value={num2}
-          onChange={onChangeNum2}
-        />
-      </div>
-      <div>
-        <button
-          onClick={onClickAdd}
-          className="border-2 px-2 py-1 rounded-lg border-red-500 text-red-500"
-        >
-          Add
-        </button>
-        <button
-          onClick={onClickSubtract}
-          className="border-2 px-2 py-1 rounded-lg border-blue-500 text-blue-500 ml-4"
-        >
-          Subtract
-        </button>
-        <button
-          onClick={onClickMultiply}
-          className="border-2 px-2 py-1 rounded-lg border-yellow-500 text-yellow-500 ml-4"
-        >
-          Multiply
-        </button>
-        <button
-          onClick={onClickDivide}
-          className="border-2 px-2 py-1 rounded-lg border-green-500 text-green-500 ml-4"
-        >
-          Divide
-        </button>
-      </div>
+    // Container
+    <div className="bg-red-100 min-h-screen">
+    <Header />
+      <main>
+          <Introduce />
+          <Portfolio />
+          <ContactMe  />
+      </main>
+          <footer />      
     </div>
   );
-};
+}
 
 export default App;
---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,70 @@
+const Portfolio = () => {
+    return (
+    
+    <ul className="bg-indigo-100 max-w-screen-xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-items-center gap-8 py-8">
+    <li className="bg-purple-100 w-60 h-72 rounded-xl">
+      <div className="bg-yellow-100 w-full h-1/2 rounded-t-xl">
+        프로젝트 이미지
+      </div>
+      <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+      <div className="mt-2 mx-2">
+        갑 든 예가 사막이다. 수 가진 고행을 따뜻한 곳으로 천고에 피다.
+        눈에 미인을 때까지 굳세게 그들의 바이며,
+      </div>
+    </li>
+    <li className="bg-purple-100 w-60 h-72 rounded-xl">
+      <div className="bg-yellow-100 w-full h-1/2 rounded-t-xl">
+        프로젝트 이미지
+      </div>
+      <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+      <div className="mt-2 mx-2">
+        갑 든 예가 사막이다. 수 가진 고행을 따뜻한 곳으로 천고에 피다.
+        눈에 미인을 때까지 굳세게 그들의 바이며,
+      </div>
+    </li>
+    <li className="bg-purple-100 w-60 h-72 rounded-xl">
+      <div className="bg-yellow-100 w-full h-1/2 rounded-t-xl">
+        프로젝트 이미지
+      </div>
+      <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+      <div className="mt-2 mx-2">
+        갑 든 예가 사막이다. 수 가진 고행을 따뜻한 곳으로 천고에 피다.
+        눈에 미인을 때까지 굳세게 그들의 바이며,
+      </div>
+    </li>
+    <li className="bg-purple-100 w-60 h-72 rounded-xl">
+      <div className="bg-yellow-100 w-full h-1/2 rounded-t-xl">
+        프로젝트 이미지
+      </div>
+      <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+      <div className="mt-2 mx-2">
+        갑 든 예가 사막이다. 수 가진 고행을 따뜻한 곳으로 천고에 피다.
+        눈에 미인을 때까지 굳세게 그들의 바이며,
+      </div>
+    </li>
+    <li className="bg-purple-100 w-60 h-72 rounded-xl">
+      <div className="bg-yellow-100 w-full h-1/2 rounded-t-xl">
+        프로젝트 이미지
+      </div>
+      <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+      <div className="mt-2 mx-2">
+        갑 든 예가 사막이다. 수 가진 고행을 따뜻한 곳으로 천고에 피다.
+        눈에 미인을 때까지 굳세게 그들의 바이며,
+      </div>
+    </li>
+    <li className="bg-purple-100 w-60 h-72 rounded-xl">
+      <div className="bg-yellow-100 w-full h-1/2 rounded-t-xl">
+        프로젝트 이미지
+      </div>
+      <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+      <div className="mt-2 mx-2">
+        갑 든 예가 사막이다. 수 가진 고행을 따뜻한 곳으로 천고에 피다.
+        눈에 미인을 때까지 굳세게 그들의 바이며,
+      </div>
+    </li>
+  </ul>
+    
+    );
+    };
+      
+      export default Portfolio;
